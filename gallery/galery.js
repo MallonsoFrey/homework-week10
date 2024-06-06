0829ed67f90a1411d2b98c74a882addc85183939
MalonsoFrey <miss.dunaewa201141@gmail.com>
2024-06-06 19:16:20 +0400

    gallery

---
+++ gallery/galery.js
@@ -0,0 +1,9 @@
+let image = document.getElementById('image')
+
+function rightImage () {
+    image.src = './assets/hedg-2.jpg'
+}
+
+function leftImage () {
+    image.src = './assets/hedg-1.jpg'
+}
